637714cab60d37ea8813c377d44a5915592254dd
Shaun Goh <43333891+SHAUNGOH97@users.noreply.github.com>
2020-03-20 00:37:03 +0800

    Update StallBuilder.java

--- src/test/java/seedu/foodiebot/testutil/StallBuilder.java
+++ src/test/java/seedu/foodiebot/testutil/StallBuilder.java
@@ -17,6 +17,8 @@ public class StallBuilder {
     public static final String DEFAULT_STALL_IMAGE = "taiwanese.png";
     public static final String DEFAULT_OVERALL_PRICERATING = "$";
     public static final int DEFAULT_IS_FAVORITE = 0;
+    public static final Tag DEFAULT_TAG = new Tag("spicy");
+    private final Set<Tag> tags = new HashSet<>();
 
     private Name name;
     private String canteenName;
@@ -25,6 +27,7 @@ public class StallBuilder {
     private String cuisine;
     private String overallPriceRating;
     private int favorite;
+    
 
 
     public StallBuilder() {
@@ -35,6 +38,7 @@ public class StallBuilder {
         this.cuisine = DEFAULT_CUISINE;
         this.overallPriceRating = DEFAULT_OVERALL_PRICERATING;
         this.favorite = DEFAULT_IS_FAVORITE;
+        this.tags.add(DEFAULT_TAG);
     }
 
     /**
@@ -48,6 +52,7 @@ public class StallBuilder {
         this.cuisine = stallToCopy.getCuisine();
         this.overallPriceRating = stallToCopy.getOverallPriceRating();
         this.favorite = stallToCopy.getFavorite();
+        this.tags.add(DEFAULT_TAG);
     }
 
     /**
@@ -114,6 +119,6 @@ public class StallBuilder {
      */
     public Stall build() {
         return new Stall(name, canteenName, stallNumber, stallImageName, cuisine,
-            overallPriceRating, favorite, new ArrayList<>());
+            overallPriceRating, favorite, tags, new ArrayList<>());
     }
 }

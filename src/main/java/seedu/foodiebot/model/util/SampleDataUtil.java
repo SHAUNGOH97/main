907c6ffa4e5d0295870b7aaf7ca5c7d86379e4e6
Shaun Goh <43333891+SHAUNGOH97@users.noreply.github.com>
2020-03-20 16:02:40 +0800

    Update SampleDataUtil.java

--- src/main/java/seedu/foodiebot/model/util/SampleDataUtil.java
+++ src/main/java/seedu/foodiebot/model/util/SampleDataUtil.java
@@ -222,7 +222,7 @@ public class SampleDataUtil {
     private static FavoriteFood[] getSampleFavoriteFood() {
         return new FavoriteFood[]{new FavoriteFood("Combo Set", 6, "1 Meat, 1 Pasta, 2 Sides", "Combo Set.png",
                 1, "Nus Flavors", "Western", getTagSet("1")),
-                new FavoriteFood("Cai Fan", 3, "1 Meat, 2 Veg", "Cai Fan.png",
+            new FavoriteFood("Cai Fan", 3, "1 Meat, 2 Veg", "Cai Fan.png",
                 1, "The Deck", "Chinese Cooked Food", getTagSet("expensive")),
             new FavoriteFood("Ayam Penyet", 5, "Ayam Penyet", "Ayam Penyet.png",
                 15, "The Deck", "Uncle Penyet", getTagSet("asian", "western", "expensive")),
